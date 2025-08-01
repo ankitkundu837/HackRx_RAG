c41d3814dd778dcbfd2468f940579ae0d351927c
ankitkundu837 <ankitkundu837@gmail.com>
2025-08-02 02:57:32 +0530

    level 2 commit

--- utils/search.py
+++ utils/search.py
@@ -10,9 +10,9 @@ def query_documents(questions: list[str]):
     print(f"Querying documents for questions: {questions}")
     
     # Query Chroma with the list of questions
-    results = collection.query(query_texts=questions, n_results=3)
+    results = collection.query(query_texts=questions, n_results=6)
 
     relevant_chunks = results["documents"]
 
-    print("==== Returning relevant chunks ====")
+    print("==== Relevant Chunks Retrieved ====")
     return relevant_chunks
